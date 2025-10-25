3026ffc5e6ea158165272948920d15f161151086
timofoi <timofeyalisov@gmail.com>
2025-10-25 14:32:43 +0300

    full vers

--- main.cpp
+++ main.cpp
@@ -1,2 +1,17 @@
+#include <iostream>
 int main(){
+	using u_t = unsigned;
+	u_t a = 0;
+	size_t k = 0;
+	while(std::cin>>a){
+		k++;
+	}
+	if (std::cin.eof()){
+		std::cout<<k;
+		std::cout<<"\n";
+	}
+	else if(std::cin.fail()){
+		std::cerr<<"Input err \n";
+		return 1;
+	}
 }
